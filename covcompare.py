8c6e100b7ba9bc28c9336d4902b3b8496883b079
Matthew Wong <wong.kl.matthew@gmail.com>
2024-08-23 02:02:50 +0800

    complete coverage comparison feature and add jupyter notebook version

--- covcompare.py
+++ covcompare.py
@@ -43,13 +43,78 @@ def parse_non_covered_lines(coverage_dict):
     return all_non_covered_lines
 
 
-def compare_coverage(fuzzer_non_covered_lines, cts_non_covered_lines):
-    pass
+def parse_covered_lines(coverage_dict):
+    all_covered_lines = {}
+
+    for file, coverage_data in coverage_dict.items():
+        file_coverage_info = {'percentage': coverage_data['p']}
+        # Make a set for the non-covered lines
+
+        covered_lines = set()
+        raw_covered_lines = coverage_data['g']
+
+        for entry in raw_covered_lines:
+            covered_lines.update(entry['s'])
+
+        file_coverage_info['covered_lines'] = covered_lines
+        all_covered_lines[file] = file_coverage_info
+
+    return all_covered_lines
+
+# def compare_coverage(fuzzer_non_covered_lines, cts_non_covered_lines):
+#     coverage_diff = {}
+#     for file, coverage in fuzzer_non_covered_lines.items():
+#         lines_not_covered_by_fuzzer = fuzzer_non_covered_lines[file]['uncovered_lines']
+#
+#         if (file in cts_non_covered_lines):
+#             lines_not_covered_by_cts = cts_non_covered_lines[file]['uncovered_lines']
+#         else:
+#             lines_not_covered_by_cts = set()
+#
+#         lines_covered_by_fuzzer_and_not_cts = lines_not_covered_by_cts.difference(lines_not_covered_by_fuzzer)
+#
+#     return coverage_diff
+
+
+def compare_coverage(fuzzer_covered_lines, cts_covered_lines):
+    coverage_diff = {}
+    for file, coverage_data in fuzzer_covered_lines.items():
+        fuzzer_percent_coverage = fuzzer_covered_lines[file]['percentage']
+        lines_covered_by_fuzzer = fuzzer_covered_lines[file]['covered_lines']
+
+        if (file in cts_covered_lines):
+            lines_covered_by_cts = cts_covered_lines[file]['covered_lines']
+            cts_percent_coverage = cts_covered_lines[file]['percentage']
+        else:
+            lines_covered_by_cts = set()
+            cts_percent_coverage = 0
+
+        # because it is offset for some reason
+        modified_lines_covered_by_fuzzer = { line_num - 336 for line_num in lines_covered_by_fuzzer}
+        lines_covered_by_fuzzer_and_not_cts = modified_lines_covered_by_fuzzer.difference(lines_covered_by_cts)
+        num_lines_covered_by_fuzzer_and_not_cts = len(lines_covered_by_fuzzer_and_not_cts)
+
+        if int(fuzzer_percent_coverage) == 0:
+            coverage_diff[file] = 0
+            continue
+        print(fuzzer_covered_lines[file])
+
+        if (cts_percent_coverage == 0):
+            total_lines = len(lines_covered_by_fuzzer) / fuzzer_percent_coverage
+        else:
+            total_lines = len(lines_covered_by_cts) / cts_percent_coverage
+
+        if num_lines_covered_by_fuzzer_and_not_cts == 0:
+            coverage_diff[file] = 0
+        else:
+            coverage_diff[file] = 100 * num_lines_covered_by_fuzzer_and_not_cts / total_lines
+
+    return coverage_diff
 
 
 def display_difference(difference_in_coverage):
     df = pd.DataFrame(list(difference_in_coverage.items()), columns=['File', 'Coverage Percentage'])
-    sorted_df = sorted_df = df.sort_values(by='File', ascending=False)
+    sorted_df = df.sort_values(by='File', ascending=False)
     print(sorted_df)
 
 
@@ -58,12 +123,12 @@ def main(fuzzer_coverage_path, cts_coverage_path):
     fuzzer_coverage_dict = load_dict_from_file(fuzzer_coverage_path)
     cts_coverage_dict = load_dict_from_file(cts_coverage_path)
 
-    # Parse all the non covered lines by file
-    fuzzer_non_covered_lines = parse_non_covered_lines(fuzzer_coverage_dict)
-    cts_non_covered_lines = parse_non_covered_lines(cts_coverage_dict)
+    # Parse all covered lines by file
+    fuzzer_covered_lines = parse_covered_lines(fuzzer_coverage_dict)
+    cts_covered_lines = parse_covered_lines(cts_coverage_dict)
 
     # Get a dictionary of the percent covered by fuzzer but not CTS
-    difference_in_coverage = compare_coverage(fuzzer_non_covered_lines, cts_non_covered_lines)
+    difference_in_coverage = compare_coverage(fuzzer_covered_lines, cts_covered_lines)
 
     # Print to dataframe
     display_difference(difference_in_coverage)
@@ -71,6 +136,6 @@ def main(fuzzer_coverage_path, cts_coverage_path):
 
 # File paths (replace these with the actual paths to your JSON files)
 fuzzer_coverage_path = './webglitch_coverage_formatted.json'
-cts_coverage_path = '/Users/matthew/Documents/msc/final_proj/cts_coverage/api_coverage_formatted.json'
+cts_coverage_path = './api_coverage_formatted.json'
 
 main(fuzzer_coverage_path, cts_coverage_path)
